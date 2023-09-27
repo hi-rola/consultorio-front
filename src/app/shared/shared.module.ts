7c939aa67f85233cee76ed1fbcecf58de9a201d0
Jose Rolando Garcia <rolaalbaa@gmail.com>
2023-09-26 18:59:28 -0600

    se crea modulo para shared

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Error404PageComponent } from './pages/error404-page/error404-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MaterialModule } from '../material/material.module';
+
+@NgModule({
+  declarations: [Error404PageComponent, HeaderComponent, FooterComponent],
+  imports: [CommonModule, MaterialModule],
+  exports: [Error404PageComponent, HeaderComponent, FooterComponent],
+})
+export class SharedModule {}
