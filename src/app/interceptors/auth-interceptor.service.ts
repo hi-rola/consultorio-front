528f7e939b4cc8bef217fcaf8d1ec42b138f4b14
Jose Rolando Garcia <rolaalbaa@gmail.com>
2023-09-29 17:53:22 -0600

    Se agrega Interceptor para agregar el token a cada petición HTTP.

---
+++ src/app/interceptors/auth-interceptor.service.ts
@@ -0,0 +1,32 @@
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, catchError, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor(private router: Router) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = localStorage.getItem('token');
+    if (token) {
+      request = request.clone({
+        headers: request.headers.set('x-token', token),
+      });
+    } else {
+      this.router.navigate(['/auth/login']);
+    }
+    return next.handle(request);
+  }
+}
