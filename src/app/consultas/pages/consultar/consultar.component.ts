db8e1c6f9c887880dd1534292bbfcb2e9a4cdad8
Jose Rolando Garcia <rolaalbaa@gmail.com>
2023-10-06 13:02:19 -0600

    Se agrega page para mostrar el form de consultar y card de información

---
+++ src/app/consultas/pages/consultar/consultar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ConsultasService } from '../../services/consultas.service';
+import { ActivatedRoute } from '@angular/router';
+import { InformacionUsuarioConsulta } from '../../interfaces/consulta.interface';
+
+@Component({
+  selector: 'consultas-consultar',
+  templateUrl: './consultar.component.html',
+  styleUrls: ['./consultar.component.css'],
+})
+export class ConsultarComponent implements OnInit {
+  public id_consulta: number | undefined;
+  public informacionUsuarioConsulta!: InformacionUsuarioConsulta;
+  public id_usuario!: string;
+
+  constructor(
+    private consultasService: ConsultasService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.id_consulta = this.activatedRoute.snapshot.params['id'];
+
+    this.consultasService
+      .getInformacionUsuarioConsulta(this.id_consulta!)
+      .subscribe((response) => {
+        this.informacionUsuarioConsulta = response;
+        this.id_usuario = response.id_usuario.toString();
+      });
+  }
+}
